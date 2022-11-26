a13f088bb755585536dc09e1d87f56debdc35d1a
Eduardo Lima <edulimacosta223@gmail.com>
2022-11-26 20:40:17 -0300

    Aula 2

--- mecajato/settings.py
+++ mecajato/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import os
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,5 +116,3 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
