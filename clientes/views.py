53645a92150e8f5b577dbdb0858fea3190537b1b
Eduardo Lima <edulimacosta223@gmail.com>
2022-11-27 14:32:04 -0300

    Aula 3

--- clientes/views.py
+++ clientes/views.py
@@ -4,6 +4,9 @@ from .models import Cliente, Carro
 import re
 from django.core import serializers
 import json
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
+from django.shortcuts import redirect
 
 
 def clientes(request):
@@ -38,12 +41,44 @@ def clientes(request):
         for carro, placa in zip(carros, placas):
             car = Carro(carro=carro, placa=placa, cliente=cliente)
             car.save()
-        return HttpResponse('teste')
+        return redirect(reverse('clientes')+f'?aba=att_clientes&id_cliente={id}')
 
 
 def att_cliente(request):
     id_cliente = request.POST.get('id_cliente')
     cliente = Cliente.objects.filter(id=id_cliente)
+    carros = Carro.objects.filter(cliente=cliente[0])
     cliente_json = json.loads(serializers.serialize('json', cliente))[
         0]['fields']
-    return JsonResponse(cliente_json)
+    carros_json = json.loads(serializers.serialize('json', carros))
+    carros_json = [{'fields': carro['fields'], 'id': carro['pk']}
+                   for carro in carros_json]
+    data = {'cliente': cliente_json, 'carros': carros_json}
+    return JsonResponse(data)
+
+
+@csrf_exempt
+def update_carro(request, id):
+    nome_carro = request.POST.get('carro')
+    placa = request.POST.get('placa')
+
+    carro = Carro.objects.get(id=id)
+    list_carros = Carro.objects.exclude(id=id).filter(placa=placa)
+    if list_carros.exists():
+        return HttpResponse('Placa já existente')
+
+    carro.carro = nome_carro
+    carro.placa = placa
+    carro.save()
+    return redirect(reverse('clientes')+f'?aba=att_clientes&id_cliente={id}')
+
+
+@csrf_exempt
+def excluir_carro(request, id):
+    try:
+        carro = Carro.objects.get(id=id)
+        carro.delete()
+        return redirect(reverse('clientes')+f'?aba=att_clientes&id_cliente={id}')
+    except:
+        # TODO: Exibir mensagem de erro
+        return redirect(reverse('clientes')+f'?aba=att_clientes&id_cliente={id}')
--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -2,7 +2,7 @@ function add_carro() {
 
     container = document.getElementById("form-carro")
 
-    html = "<br> <div class='row'> <div class='col-md'> <input type='text' placeholder='carro' class='form-control' name='carro'> </div> <div class='col-md'><input type='text' placeholder='Placa' class='form-control' name='placa'> </div> </div>"
+    html = "<br> <div class='row'> <div class='col-md'> <input type='text' required='required' placeholder='carro' class='form-control' name='carro'> </div> <div class='col-md'><input type='text' required='required' placeholder='Placa' class='form-control' name='placa'> </div> </div>"
 
     container.innerHTML += html
 
@@ -41,19 +41,24 @@ function dados_cliente() {
         return result.json()
     }).then(function (data) {
         document.getElementById('form-att-cliente').style.display = 'block'
-        console.log(data)
         nome = document.getElementById('nome')
-        nome.value = data['nome']
+        nome.value = data['cliente']['nome']
 
         sobrenome = document.getElementById('sobrenome')
-        sobrenome.value = data['sobrenome']
+        sobrenome.value = data['cliente']['sobrenome']
 
         cpf = document.getElementById('cpf')
-        cpf.value = data['cpf']
+        cpf.value = data['cliente']['cpf']
 
         email = document.getElementById('email')
-        email.value = data['email']
+        email.value = data['cliente']['email']
 
+        div_carros = document.getElementById('carros')
+        div_carros.innerHTML = ""
+        for (i = 0; i < data['carros'].length; i++) {
+            div_carros.innerHTML += "<form action='/clientes/update_carro/" + data['carros'][i]['id'] + "' method='POST'><div class='row justify-content-md-center'><div class='col-md-auto'><input class='form-control' type='text' name='carro' value='" + data['carros'][i]['fields']['carro'] + "'</div></div><div class='col-md-auto'><input class='form-control' type='text' name='placa' value='" + data['carros'][i]['fields']['placa'] + "'/></div><div class='col-md-auto'><input class='btn btn-success' type='submit' value='Salvar'/></div><div class='col-md-auto'><a href='/clientes/excluir_carro/" + data['carros'][i]['id'] + "' class='btn btn-warning'>Excluir</a></div></div></div></form><br>"
+
+        }
     })
 
 }
